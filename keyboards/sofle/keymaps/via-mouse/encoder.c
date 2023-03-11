3a08eef80afbb3fe7de2c7e3ac79c1847f81efd2
Martin Palecek <martin.palecek@ketry.net>
2023-03-11 23:54:33 +0100

    Change mapping of encoders

--- keyboards/sofle/keymaps/via-mouse/encoder.c
+++ keyboards/sofle/keymaps/via-mouse/encoder.c
@@ -22,15 +22,15 @@
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code16(LSFT(KC_VOLU));
         } else {
-            tap_code(KC_VOLD);
+            tap_code16(LSFT(KC_VOLD));
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_MS_WH_UP);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_MS_WH_DOWN);
         }
     }
     return true;
