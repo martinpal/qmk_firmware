407edc359f196aff039b84d656b336fe03b9e4e4
Martin Palecek <martin.palecek@ketry.net>
2023-03-16 22:17:41 +0100

    Reset default layers states for all layers

--- keyboards/sofle/keymaps/via-mouse/keymap.c
+++ keyboards/sofle/keymaps/via-mouse/keymap.c
@@ -188,7 +188,6 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 );
 
 void keyboard_post_init_user(void) {
-    // Enable the LED layers
     rgblight_layers = my_rgb_layers;
 }
 
@@ -200,7 +199,9 @@ bool led_update_user(led_t led_state) {
 }
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, 0));
+    for (int layer=0;layer<7;layer++) {
+        rgblight_set_layer_state(layer, 0);
+    }
     return state;
 }
 
