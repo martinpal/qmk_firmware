92abc5bca06fa7c2211263c9d65f205af73c07da
Martin Palecek <martin.palecek@ketry.net>
2023-07-01 11:04:12 +0200

    More home row mods

--- keyboards/sofle/keymaps/via-mouse/keymap.c
+++ keyboards/sofle/keymaps/via-mouse/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
    QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
-KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,      KC_H,RSFT_T(KC_J),RALT_T(KC_K),RCTL_T(KC_L),KC_SCLN,  KC_QUOT,
+KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,     KC_H,RSFT_T(KC_J),RALT_T(KC_K),RCTL_T(KC_L),KC_SCLN,KC_QUOT,
    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,     KC_NO, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
         KC_LGUI,LALT_T(KC_LBRC),KC_LCTL, MO(2),LSFT_T(KC_ENT),          KC_SPC,MO(3), KC_RCTL,RALT_T(KC_RBRC), KC_APP
 ),
@@ -67,7 +67,7 @@ KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,      KC_H,RSFT_
         KC_LGUI,LALT_T(KC_LBRC),KC_LCTL, MO(2),LSFT_T(KC_ENT),          KC_SPC,MO(3), KC_RCTL,RALT_T(KC_RBRC), KC_APP
 ),
 /*
- * LOWER - RED
+ * LOWER - RED (numbers, symbols with home row mods)
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -85,7 +85,7 @@ KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,      KC_H,RSFT_
 [2] = LAYOUT(
     KC_GRV, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
    QK_GESC, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-    KC_TAB,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
+KC_TAB,LGUI_T(KC_1),LCTL_T(KC_2),LALT_T(KC_3),LSFT_T(KC_4),KC_5,       KC_6,RSFT_T(KC_7),RALT_T(KC_8),RCTL_T(KC_9),KC_0,KC_F12,
    SC_LSPO,KC_EQL,KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_MUTE,     KC_NO, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, SC_RSPC,
                 KC_RGUI,KC_LALT,KC_LCTL,KC_TRNS,LSFT_T(KC_SPC),          KC_SPC,MO(5), KC_RCTL, KC_RALT, KC_APP
 ),
@@ -149,7 +149,7 @@ KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,      KC_H,RSFT_
     KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,                           KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
    QK_BOOT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3,   KC_NO,
     QK_RBT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4,   KC_NO,
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   DF(0),   KC_NO,          KC_NO,   DF(1), KC_WH_L,   KC_NO, KC_WH_R, KC_BTN5,   KC_NO,
+QK_CLEAR_EEPROM,KC_NO, KC_NO,   KC_NO,   KC_NO,   DF(0),   KC_NO,          KC_NO,   DF(1), KC_WH_L,   KC_NO, KC_WH_R, KC_BTN5,   KC_NO,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,LSFT_T(KC_ENT),                 KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* EXTRA - MAGENTA
