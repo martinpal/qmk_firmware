857a7a5a4bb1b5c20936fc57499f746353da4c5a
Martin Palecek <martin.palecek@ketry.net>
2023-10-02 21:59:45 +0200

    Tweak lower row mods
    This way pressing modifiers with some keys is made easier with one hand. For
    example Shift+F can now be pressed with middle finger and pointer finger of
    the left hand (C+F).

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// Base
   [0] = LAYOUT_split_3x6_3(
    QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
 KC_TAB,LGUI_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LSFT_T(KC_F),KC_G,     KC_H,RSFT_T(KC_J),RALT_T(KC_K),RCTL_T(KC_L),KC_SCLN,KC_QUOT,
@@ -27,6 +28,7 @@ KC_LSFT,LGUI_T(KC_Z),LCTL_T(KC_X),LALT_T(KC_C),LSFT_T(KC_V),KC_B,      KC_N,    
                                  KC_LGUI,MO(2),LSFT_T(KC_ENT),          KC_SPC,MO(3),  KC_APP
   ),
 
+// Game
   [1] = LAYOUT_split_3x6_3(
    QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
     KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
@@ -34,20 +36,23 @@ KC_LSFT,LGUI_T(KC_Z),LCTL_T(KC_X),LALT_T(KC_C),LSFT_T(KC_V),KC_B,      KC_N,    
                                  KC_LGUI,MO(2),LSFT_T(KC_ENT),          KC_SPC,MO(3), KC_APP
   ),
 
+// Lower
   [2] = LAYOUT_split_3x6_3(
     KC_GRV, KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
 KC_TAB,LGUI_T(KC_1),LCTL_T(KC_2),LALT_T(KC_3),LSFT_T(KC_4),KC_5,       KC_6,RSFT_T(KC_7),RALT_T(KC_8),RCTL_T(KC_9),KC_0,KC_F12,
-SC_LSPO,LGUI_T(KC_EQL),LCTL_T(KC_MINS),LALT_T(KC_PLUS),LSFT_T(KC_LBRC),KC_RBRC,     KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, SC_RSPC,
+SC_LSPO,LGUI_T(KC_EQL),LALT_T(KC_MINS),LSFT_T(KC_PLUS),LCTL_T(KC_LBRC),KC_RBRC,     KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, SC_RSPC,
                 KC_RGUI,KC_TRNS,LSFT_T(KC_SPC),          KC_SPC,MO(5), KC_APP
   ),
 
+// Raise
   [3] = LAYOUT_split_3x6_3(
   KC_ESC, KC_INS,KC_PSCR,  KC_NO,LCTL(KC_R), KC_SCRL,                      KC_PGUP,C(KC_LEFT), KC_UP,C(KC_RGHT), KC_NO,  KC_F12,
  KC_TAB,LCTL(KC_A),KC_LCTL,KC_LALT, KC_LSFT, KC_CAPS,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
-SC_LSPO,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V),KC_NUM,                KC_NO, KC_HOME,   KC_NO,  KC_END,   KC_NO, SC_RSPC,
+SC_LSPO,LCTL(KC_Z),LALT(KC_X),LSFT(KC_C),LALT(KC_V),KC_NUM,                KC_NO, KC_HOME,   KC_NO,  KC_END,   KC_NO, SC_RSPC,
                                      KC_TRNS,MO(4),LSFT_T(KC_ENT),         KC_ENT, KC_TRNS, KC_TRNS
   ),
 
+// Adjust
   [4] = LAYOUT_split_3x6_3(
   RGB_RMOD,   KC_NO, KC_PSCR,   KC_NO,    KC_NO,   KC_NO,                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MC_12,
    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,    KC_NO,   KC_NO,                           KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,   MC_13,
@@ -55,13 +60,15 @@ SC_LSPO,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V),KC_NUM,                KC_NO
                                    KC_TRNS,KC_TRNS, LSFT_T(KC_ENT),                KC_SPC, KC_TRNS, KC_TRNS
   ),
 
+// Mouse
   [5] = LAYOUT_split_3x6_3(
-   QK_BOOT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3,   KC_NO,
-    QK_RBT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                          KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4,   KC_NO,
-QK_CLEAR_EEPROM,KC_NO, KC_NO,   KC_NO,   KC_NO,   DF(0),                            DF(1), KC_WH_L,   KC_NO, KC_WH_R, KC_BTN5,   KC_NO,
+   QK_BOOT,    MC_1,    MC_2,    MC_3,    MC_4,    MC_5,                          KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3,   KC_NO,
+    QK_RBT,    MC_6,    MC_7,    MC_8,    MC_9,   MC_10,                          KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4,   KC_NO,
+QK_CLEAR_EEPROM,MC_11, MC_12,   MC_13,   MC_14,   DF(0),                            DF(1), KC_WH_L,   KC_NO, KC_WH_R, KC_BTN5,   KC_NO,
                                   KC_TRNS, KC_TRNS,LSFT_T(KC_ENT),                 KC_SPC, KC_TRNS, KC_TRNS
   ),
 
+// Extra
   [6] = LAYOUT_split_3x6_3(
      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
